059100ceb89310410163d045fcb64a0f2cd9a05d
劉哲宇 <j2325138@gmail.com>
2017-04-09 01:00:31 +0800

    What the hell I just modified

--- myTest/1.c
+++ myTest/1.c
@@ -1,16 +1,2 @@
-   /* Alo
-*/      #pragma source /*asd
-f*/ on
-123
-0
-122/*12345*/hey
-int a = 20;// yolo
-//1.3
-1e2
-1E4
-2222
-	int a
-1.e1
-"string"
-'c' 'h' 'a' 'r'
-2.3e2.3
+	j = 2.e00;
+	
--- myTest/2.c
+++ myTest/2.c
@@ -4,3 +4,10 @@ adsf
 hey
 3.0e
 e2.3
+	// TYO
+#pragma source on //heyhey
+	int a;// heyhey
+	double third/*hahahahaha
+	so cool
+	*/+second/*GGGGGGG*/+a++;
+	double i,j,k;
